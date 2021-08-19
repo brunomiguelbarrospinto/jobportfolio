4facf1c47a850a8b05da074e9e921dc6f89e1b13
Bruno Barros <bruno@filmin.es>
2021-08-19 23:25:24 +0200

    Studies module in progress

---
+++ src/components/studies/StudiesList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-white border p-4">
+    <div class="flex justify-between">
+      <div>Mis estudios</div>
+      <Button :to="{ name: 'dashboard-studies-create' }">Añadir</Button>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+// TODO: create list components, create useStudies to get, create, update amd delete studies
+
+import { defineComponent } from "vue";
+import Button from "@/components/common/button/Button.vue";
+export default defineComponent({
+  components: {
+    Button,
+  },
+});
+</script>
