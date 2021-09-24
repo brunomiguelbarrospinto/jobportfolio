1e06f2909bf08e87648125565899f4029d385738
Bruno Barros <bruno@filmin.es>
2021-09-24 03:17:04 +0200

    Storybook with button sizes story

---
+++ config/storybook/main.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  stories: ["../../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-links",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        cssLoaderOptions: {
+          // When you have splitted your css over multiple files
+          // and use @import('./other-styles.css')
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          // When using postCSS 8
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
+    });
+    return config;
+  },
+};
--- src/components/common/button/Button.vue
+++ src/components/common/button/Button.vue
@@ -1,24 +1,5 @@
 <template>
-  <InteractiveElement
-    class="
-      text-sm
-      border border-green-500
-      bg-green-500
-      text-white
-      rounded
-      px-3
-      py-1
-      transition
-      duration-500
-      ease
-      select-none
-      hover:bg-green-600
-      focus:outline-none focus:shadow-outline
-      flex
-      items-center
-    "
-    :disabled="isDisabled"
-  >
+  <InteractiveElement :class="classList" :disabled="isDisabled">
     <svg
       v-if="isLoading"
       class="animate-spin -ml-1 mr-2 h-4 w-4 text-white"
@@ -45,8 +26,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
 import InteractiveElement from "@/components/common/InteractiveElement.vue";
+
+export const sizes = ["xs", "sm", "md", "lg", "xl"];
+export const colors = ["primary"];
 
 export default defineComponent({
   components: {
@@ -55,10 +39,122 @@ export default defineComponent({
   props: {
     isDisabled: {
       type: Boolean,
+      default: false,
     },
     isLoading: {
       type: Boolean,
+      default: false,
     },
+    isOutline: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: "md",
+      validator: (value: string) => {
+        return sizes.includes(value);
+      },
+    },
+    color: {
+      type: String,
+      validator: (value: string) => {
+        return colors.includes(value);
+      },
+    },
+  },
+  setup(props) {
+    const classList = computed(() => {
+      const { isOutline, color } = props;
+      const isOutlineClass = isOutline ? "button--outline" : "";
+      const colorClass = color ? `button--${color}` : "";
+      return `button button--${props.size} ${isOutlineClass} ${colorClass}`;
+    });
+    return {
+      classList,
+    };
   },
 });
 </script>
+
+<style lang="scss">
+$default-color: "gray-500";
+$default-hover-color: "gray-700";
+$default-focus-color: "gray-800";
+.button {
+  // Basic
+  @apply cursor-pointer;
+  // Display
+  @apply flex items-center;
+  // Text
+  @apply text-#{$default-color} font-medium;
+  // Background
+  @apply bg-gray-100;
+  // Border
+  @apply rounded border-0;
+  // Transition
+  @apply transition duration-500;
+
+  // Disabled
+  &[disabled],
+  &[disabled="disabled"],
+  &:disabled {
+    @apply opacity-50 pointer-events-none;
+  }
+
+  // Sizes
+  &--xs {
+    @apply text-xs;
+    @apply h-6 px-2;
+  }
+  &--sm {
+    @apply text-sm;
+    @apply h-8 px-3;
+  }
+  &--md {
+    @apply text-base;
+    @apply h-10 px-4;
+  }
+  &--lg {
+    @apply text-lg;
+    @apply h-12 px-5;
+  }
+  &--xl {
+    @apply text-xl;
+    @apply h-14 px-6;
+  }
+
+  // Hover
+  &:hover {
+    @apply hover:bg-gray-200 hover:text-#{$default-hover-color};
+  }
+
+  &:focus {
+    @apply focus:bg-gray-300  focus:text-#{$default-focus-color};
+  }
+
+  &--outline {
+    @apply border border-#{$default-color};
+    &:hover {
+      @apply border-#{$default-color};
+    }
+    &:focus {
+      @apply focus:border-#{$default-focus-color};
+    }
+  }
+
+  &--primary {
+    // Background
+    @apply bg-blue-500;
+    // Text
+    @apply text-white;
+
+    &:hover {
+      @apply hover:bg-blue-600 hover:text-white;
+    }
+    &:focus {
+      @apply focus:bg-blue-700 focus:text-white;
+    }
+  }
+}
+</style>
